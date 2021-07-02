7dbeb1656deba885ed057c8276e4571068b6b027
Maiamona Garcia <maiamona.garcia@jupiter.co.ao>
2021-07-02 11:02:50 +0100

    Aula completa angularjs

---
+++ bootstrap/js/novo.js
@@ -0,0 +1,14 @@
+window.addEventListener('hashchange', function(){
+	
+	if(window.location.hash === '#/bookmark/1'){
+		
+		console.log('Pagina 1 é legal');
+	}
+	
+		if(window.location.hash === '#/bookmark/2'){
+		
+		console.log('Pagina 2 atualizada há 7 minutos');
+	}
+	
+	
+}) 
